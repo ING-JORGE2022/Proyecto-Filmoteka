dff5e6509675dd87df696528d817953e955667a9
Carlos Andres Castrillon <caacastr@bancolombia.com.co>
2023-05-15 20:42:01 -0500

    Opcion Buscar palabra Clave

--- src/js/rendering-movie-cards.js
+++ src/js/rendering-movie-cards.js
@@ -1,4 +1,5 @@
 import { getTrending } from './api';
+import { searchMovieName } from './api';
 import { createGalleryPage } from './create-gallery-page';
 import { createPagination } from './pagination';
 import refs from './ref';
@@ -21,3 +22,32 @@ getTrending().then(data => {
     })
   })
 });
+
+const input = document.querySelector('.search__input');
+const btnSearch = document.querySelector('.bx-search');
+
+btnSearch.addEventListener('click', onSearchBtn);
+
+function onSearchBtn(e){
+  e.preventDefault();
+  let movie = input.value.trim();
+console.log(movie);
+  searchMovieName(movie)
+  .then(data => {
+       if (!data.total_results) {
+       setTimeout(() => {
+         console.log("Search result found")
+                   }, 5000);
+       console.log('Search result not successful.');
+       return;
+     }
+     console.log(data.results);
+     galleryMovie.innerHTML="";
+     galleryMovie.insertAdjacentHTML(
+      'beforeend',
+      createGalleryPage(data.results)
+    );
+
+   })
+   .catch(error => console.log(error));
+}
