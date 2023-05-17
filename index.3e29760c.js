e6456f09c2d1e92caadfd224be44d6dad3c2ae81
ING-JORGE2022 <116320081+ING-JORGE2022@users.noreply.github.com>
2023-05-17 21:38:06 +0000

    Deploying to gh-pages from @ ING-JORGE2022/Proyecto-Filmoteka@e8cb659c38a701210c19acfe71de947fd2565483 🚀

---
+++ index.3e29760c.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".botton_to_top").click((function(){$("body, html").animate({scrollTop:"0px"},300)})),$(window).scroll((function(){$(this).scrollTop()>0?$(".botton_to_top").slideDown(300):$(".botton_to_top").slideUp(300)}))}));
+//# sourceMappingURL=index.3e29760c.js.map
